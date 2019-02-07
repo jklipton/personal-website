feb47129f8de76878c4878520fbbe6d385874b36
Lipton <jkl.lipton@gmail.com>
2019-02-07 11:31:48 -0800

    New build

---
+++ src/components/Layout.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Footer from './Footer'
+
+import '../assets/scss/main.scss'
+
+class Layout extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isMenuVisible: false,
+            loading: 'is-loading'
+        }
+        this.handleToggleMenu = this.handleToggleMenu.bind(this)
+    }
+
+    componentDidMount () {
+        this.timeoutId = setTimeout(() => {
+            this.setState({loading: ''});
+        }, 100);
+    }
+
+    componentWillUnmount () {
+        if (this.timeoutId) {
+            clearTimeout(this.timeoutId);
+        }
+    }
+
+    handleToggleMenu() {
+        this.setState({
+            isMenuVisible: !this.state.isMenuVisible
+        })
+    }
+
+    render() {
+        const { children } = this.props
+
+        return (
+            <div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
+                <div id="wrapper">
+                    {children}
+                    <Footer />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Layout
+
+//TODO: Refactor to match pattern
